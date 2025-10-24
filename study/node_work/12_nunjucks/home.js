43308587529d6807769b1a5ee02fd7c12768a4d7
DESKTOP-7CHP8SF\user <jx007s@nate.com>
2025-10-24 10:08:19 +0900

    10_24_7

--- study/node_work/12_nunjucks/home.js
+++ study/node_work/12_nunjucks/home.js
@@ -9,13 +9,12 @@ nunjucks.configure('views', // views 폴더 이하 위치
 })
 
 app.get('/',(req,res)=>{
-    res.render("home.html") // 정적인경우 html 가능
-                            // 동적 처리를 위해서는 njk  필요
+    res.render("home.html") 
+                            
 })
 
 app.get('/01_basic',(req,res)=>{
-    res.render("01_basic.njk") // 정적인경우 html 가능
-                                // 동적 처리를 위해서는 njk  필요
+    res.render("01_basic.html")
 })
 
 let ddd = {
@@ -26,8 +25,14 @@ let ddd = {
 }
 
 app.get('/02_data',(req,res)=>{
-    res.render("02_data.njk", ddd) //  data 를 같이 전송 - 변수로 사용됨
+    res.render("02_data.html", ddd) 
 })
+
+
+app.get('/03_include',(req,res)=>{
+    res.render("03_include.html", ddd) //  data 를 같이 전송 - 변수로 사용됨
+})
+
 
 app.listen(80,()=>{
     console.log("nunjucks 서버 시작")
