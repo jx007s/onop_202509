37de0641156a4fe6d61f99899c37b408933e6b82
nemo <jx007s@nate.com>
2025-11-18 14:33:13 +0900

    11_18_4

---
+++ study/react_workkk/firstprj/src/basic/DynamicStud.js
@@ -0,0 +1,78 @@
+class Stud{
+
+    constructor(kind, name,...jum){
+        this.kind = kind
+        this.name = name
+        this.jum = jum
+
+        this.totCalc()
+        this.calc()
+    }
+
+    totCalc(){
+        this.tot = 0
+        for (const j of this.jum) {
+            this.tot += j
+        }
+    }
+
+    calc(){}
+
+    ppp(i){
+        
+        return <div key={i}>{this.kind} {this.name} {this.tot} {this.avg}</div>
+    }
+}
+
+class GenStud extends Stud{
+    constructor(name, kor, eng, mat){
+        super('일반',name, kor, eng, mat) //부모생성자 --> 자식클래스에서 생성자를 명시할 경우 부모생성자 필수 지정해야 한다.
+    }
+    calc(){  
+        this.avg = this.tot / this.jum.length
+    }
+}
+
+class ArtStud extends Stud{
+    constructor(name, kor, eng, mat, art){
+        super('예능',name,kor, eng, mat, art) //부모생성자 --> 자식클래스에서 생성자를 명시할 경우 부모생성자 필수 지정해야 한다.
+       
+    }
+    calc(){
+        
+        this.avg = 0
+        let rate = [0.05,0.1,0.15,0.7]
+        for (const j in this.jum) {
+            this.avg += this.jum[j] * rate[j]
+        }    
+    }
+}
+
+
+
+let sts = [
+    new GenStud('차은우',38,69,92), 
+    new ArtStud('차동우',38,59,72,94), 
+    new GenStud('차철우',98,69,32), 
+    new ArtStud('차금우',98,79,52,34)
+]
+
+function wefef(){
+
+    return <>
+        <h1>다형성 학생입니다.</h1>
+        {sts.map((v,k)=>v.ppp(k))}
+    </>
+}
+
+export default wefef
+
+
+/*
+부모클래스 - 도형
+도형이름, 넓이, 둘레
+
+자식클래스 - 직사각형, 원
+
+직사각형2, 원3  정보출력하세요
+*/
--- study/react_workkk/firstprj/src/index.js
+++ study/react_workkk/firstprj/src/index.js
@@ -15,7 +15,8 @@ import ReactDOM from 'react-dom/client';
 //import Extends from './basic/Extends'
 //import ExStud from './basic/ExtendsStud'
 //import ExShape from './basic/ExtendsShape'
-import Dyn from './basic/Dynamic'
+//import Dyn from './basic/Dynamic'
+import DyStud from './basic/DynamicStud'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -35,8 +36,9 @@ root.render(
     <NovDiary/>
     <Extends/>
     <ExStud/>
-    <ExShape/>*/}
-    <Dyn/>
+    <ExShape/>
+    <Dyn/>*/}
+    <DyStud/>
     {/* </React.StrictMode> */}
   </>
 );
