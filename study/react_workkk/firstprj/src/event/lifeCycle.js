7871dce85ac471101846d8ea3bb5111d7d6188d4
nemo <jx007s@nate.com>
2025-11-21 15:09:37 +0900

    11_21_3

--- study/react_workkk/firstprj/src/event/lifeCycle.js
+++ study/react_workkk/firstprj/src/event/lifeCycle.js
@@ -27,15 +27,21 @@ class LifeCycle extends Component{
         return null //변경 필요없음
     }
 
+
+    //이벤트핸들러
+    fn_1 = (e)=>{
+        console.log('window : ', window.innerWidth, window.innerHeight)
+    }
+
     // DOM 접근 이벤트/타이머/네트워크 등 side-effect 처리 : 마운트 최초 결합시 1번 실행 (업데이트시에는 실행하지 않음)
     componentDidMount(){
-        console.log('4. componentDidMount==========')
-        window.addEventListener('resize',(e)=>{console.log('window : ', window.innerWidth, window.innerHeight)})
+        console.log('5. componentDidMount==========')
+        window.addEventListener('resize',this.fn_1)
     }
 
     //update (setState() 실행) 시 호출 : render() 메소드 이전 실행
     shouldComponentUpdate(nextProps, nextState){
-        console.log("2-1. shouldComponentUpdate=============")
+        console.log("3. shouldComponentUpdate=============")
         console.log( 'nextProps', nextProps)    //부모가 보내는 변경시키려는(미래의) props
         console.log( 'nextState', nextState)    //자신의 변경시키려는(미래의) state
         
@@ -50,7 +56,7 @@ class LifeCycle extends Component{
 
     // update -> render 이후 실행 : DOM 업데이트 직전 상태를 snapshot으로 기록 --> componentDidUpdate() 에 전송
     getSnapshotBeforeUpdate(prevProps, prevState){
-        console.log("4. getSnapshotBeforeUpdate=============")
+        console.log("6. getSnapshotBeforeUpdate=============")
         console.log( 'prevProps', prevProps)    //변경전 props
         console.log( 'prevState', prevState)    //변경전 state  : cnt 1-> 2 변경시  이전값인 1 호출
         let data =  {
@@ -60,16 +66,23 @@ class LifeCycle extends Component{
         return data
     }
 
+    // DOM 업데이트 완료 후 실행
     componentDidUpdate(prevProps, prevState, snapshot){
-        console.log("5. componentDidUpdate=============")
+        console.log("7. componentDidUpdate=============")
         console.log( 'prevProps', prevProps)    //변경전 props
         console.log( 'prevState', prevState)    //변경전 state  : cnt 1-> 2 변경시  이전값인 1 호출
         console.log( 'snapshot', snapshot)  //getSnapshotBeforeUpdate 으로 부터 전송받은 data
     }
+
+    // DOM 접근 이벤트/타이머/네트워크 외부 리소스 정리 : 마운트 결합 해제시 실행
+    componentWillUnmount(){
+        console.log('8. componentWillUnmount==========')
+        window.removeEventListener('resize',this.fn_1)
+    }
     
 
     render(){
-        console.log('3. render==========')
+        console.log('4. render==========')
         return <>
             <h1>lifeCycle 입니다</h1>
             <div>prop.pVV : {this.props.pVV}</div>
