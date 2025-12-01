cd904b9dafdd406042aa953a445ed5c5235c5e0f
nemo <jx007s@nate.com>
2025-12-01 18:56:58 +0900

    12_1_5

--- study/java_work/javaprj/src/control_p/ForMain.java
+++ study/java_work/javaprj/src/control_p/ForMain.java
@@ -11,9 +11,77 @@ public class ForMain {
 		System.out.print("문어");
 		System.out.println();
 		
-		for (int big = 0; big <5; big++) {
+		int limit = 5;
+		System.out.println("1. >>>");
+		
+		for (int big = 0; big <limit; big++) {
 			
 			for (int star = 0; star <=big; star++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("2. >>>");
+		
+		for (int big = 0; big <limit; big++) {
+			
+			for (int star = big; star <limit; star++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("3. >>>");
+		
+		for (int big = 0; big <limit; big++) {
+			
+			for (int star = big; star <limit-1; star++) {
+				System.out.print(" ");
+			}
+			for (int star = 0; star <=big; star++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("4. >>>");
+		
+		for (int big = 0; big <limit; big++) {
+			
+			for (int star = 0; star <big; star++) {
+				System.out.print(" ");
+			}
+			for (int star = big; star <limit; star++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		
+		
+		System.out.println("5. >>>");
+		
+		for (int big = 0; big <limit; big++) {
+			
+			for (int star = big; star <limit-1; star++) {
+				System.out.print(" ");
+			}
+			for (int star = 0; star <=big*2; star++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		
+		System.out.println("6. >>>");
+		
+		for (int big = 0; big <limit; big++) {
+			
+			for (int star = 0; star <big; star++) {
+				System.out.print(" ");
+			}
+			for (int star = 0; star <(limit-big)*2-1; star++) {
 				System.out.print("*");
 			}
 			System.out.println();
