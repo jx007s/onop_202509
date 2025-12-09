f6c2583486e5c555bde02b5ef7f8479ce93b685e
nemo <jx007s@nate.com>
2025-12-09 11:58:13 +0900

    12_9_4

--- study/java_work/javaprj/src/inter_p/RestaurantMain.java
+++ study/java_work/javaprj/src/inter_p/RestaurantMain.java
@@ -24,35 +24,62 @@ interface Kitchen{
 
 interface Restaurant extends Counter, Hall,Kitchen{}
 
-class Alba implements Restaurant{
-	String name;
-	int [] res = new int[4];
+class AlbaData{
+		
+	AlbaData(String name) {
 
-	Alba(String name) {
 		this.name = name;
+	}
+
+	String name;
+	
+	String [] title = {"인사","계산","서빙","설거지"};
+	int [] res = new int[title.length];
+	
+	void ppp() {
+		System.out.println(name+">>>>>");
+		for (int i = 0; i < res.length; i++) {
+			System.out.println(title[i]+" : "+res[i]);
+		}
+	}
+}
+
+class Alba implements Restaurant{
+	
+	AlbaData myData;
+	AlbaData totData;
+
+	Alba(String name, AlbaData totData ) {
+		myData = new AlbaData(name);
+		this.totData = totData;
 	}
 	
 	@Override
 	public void hello() {
-		res[0]++;
-		System.out.println(name+" 인사 해요");
+		myData.res[0]++;
+		totData.res[0]++;
+		System.out.println(myData.name+" 인사 해요");
 	}
 	
 	@Override
 	public void cash(int table, int money) {
-		res[1]++;
-		System.out.println(name+" 테이블 : "+table+" "+money+" 계산해요");
+		myData.res[1]++;
+		totData.res[1]++;
+		System.out.println(myData.name+" 테이블 : "+table+" "+money+" 계산해요");
 	}
 
 	@Override
 	public void serving(int table, String menu) {
-		res[2]++;
-		System.out.println(name+" 테이블 : "+table+" "+menu+" 서빙해요");
+		myData.res[2]++;
+		totData.res[2]++;
+		System.out.println(myData.name+" 테이블 : "+table+" "+menu+" 서빙해요");
 	}
 
 	@Override
 	public void wash(int dish) {
-		System.out.println(name+" 그릇 "+dish+" 설거지 해요");
+		myData.res[3]++;
+		totData.res[3]++;
+		System.out.println(myData.name+" 그릇 "+dish+" 설거지 해요");
 	}
 	
 	
@@ -62,11 +89,14 @@ class Alba implements Restaurant{
 public class RestaurantMain {
 
 	public static void main(String[] args) {
+		
+		AlbaData totData = new AlbaData("김밥천국");
+		
 		Alba [] albas = {
-			new Alba("장동민"),
-			new Alba("로브슨"),
-			new Alba("윤성훈"),
-			new Alba("최현수")
+			new Alba("장동민",totData),
+			new Alba("로브슨",totData),
+			new Alba("윤성훈",totData),
+			new Alba("최현수",totData)
 		};
 
 		Counter cc;
@@ -82,6 +112,37 @@ public class RestaurantMain {
 		cc.cash(2, 7000);
 		hh = albas[3];
 		hh.serving(3, "부대찌개");
+		
+		for (Alba alba : albas) {
+			alba.myData.ppp();
+		}
+		System.out.println("===============");
+		totData.ppp();
 	}
 
 }
+
+/*
+스타크래프트를 구현하세요
+
+scv -4기를 이용하여 작업
+커맨드센터 : 미네랄 400
+팩토리 : 미네랄 200, 가스 100
+스타포트 : 미네랄 150, 가스 100
+
+scv 1회 획득량  미네랄 : 8, 가스 : 8
+
+
+커맨드 센터 --2 짓기
+스타포트-1 짓기 1  수리
+팩토리-2 짓기
+미네랄-500
+탱크-2대 수리
+탱크 타기 -3
+저그 공격 : 5마리
+
+
+각 작업간 log 기록을 남기고
+각 scv 의 작업결과와
+전체 상황 log를 기록하세요
+*/
